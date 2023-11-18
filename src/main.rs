36cb03c3fa3b8658839ec42afa6918c2c60e6789
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-18 12:05:18 +0530

    random number and random number within range

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+#![allow(unused_assignments)]
+
+use rand::prelude::*;
+
+fn main() {
+    let mut rng: ThreadRng = rand::thread_rng();
+    rng = generate_random_numbers(rng);
+    rng = generate_with_range(rng, 0, 10);
+}
+
+
+fn generate_random_numbers(mut rng: ThreadRng) -> ThreadRng {
+    prints("Generate random numbers");
+
+    let n1: u8 = rng.gen();
+    let n2: u16 = rng.gen();
+
+    println!("Random u8: {}", n1);
+    println!("Random u16: {}", n2);
+    println!("Random u32: {}", rng.gen::<u32>());
+    println!("Random i32: {}", rng.gen::<i32>());
+    println!("Random float: {}", rng.gen::<f64>());
+
+    rng
+}
+
+fn generate_with_range(mut rng:ThreadRng, start: i32, end: i32) -> ThreadRng {
+    prints("Generate within range");
+    let n1 = rng.gen_range(start..end);
+    println!("{}", n1);
+
+    rng
+}
+
+fn prints(value: &str) {
+    println!("\n-----------------------------------{}-------------------------------------", value);
+}
